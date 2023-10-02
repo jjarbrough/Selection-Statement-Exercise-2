d316fdf38f253b8f0caa09459d7928fe9b461c20
James Yarbrough <jjarbrough@gmail.com>
2023-10-02 12:28:48 -0500

    finished exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What was your favorite subject in school? (please don't capitilize your answer)");
+            string favSubject = Console.ReadLine();
+            switch (favSubject)
+            {
+                case "math":
+                    Console.WriteLine("I love math too!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science was my favorite too!");
+                    break;
+                case "english":
+                    Console.WriteLine("boo! english was my least favorite!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is cool");
+                    break;
+                case "programming":
+                    Console.WriteLine("programming is my all time fav!");
+                    break;
+                default: 
+                    Console.WriteLine("you must have liked P.E.");
+                    break;
+            }
         }
     }
 }
